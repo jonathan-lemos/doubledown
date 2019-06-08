f353a148c0019c34fdc91f462fc948af5c6c98da
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-06-07 21:54:01 -0400

    api key

--- main.R
+++ main.R
@@ -1,7 +1,19 @@
-if (!require(readr)) {
-	install.packages("readr")
-	library(readr)
+import <- function(name) {
+	if (!require(name, character.only = TRUE)) {
+		install.packages(name)
+		library(name, character.only = TRUE)
+	}
 }
 
-temp <- read.csv("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo&datatype=csv")
-print(temp)
+import("readr")
+import("dplyr")
+import("ggplot2")
+
+source(file = "api_key.R")
+
+get_csv <- function(symbol) {
+	link <- paste("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=", symbol, "&datatype=csv&apikey=", api_key, sep = "")
+	link %>% read.csv %>% return
+}
+
+get_csv("TSLA") %>% head %>% print
