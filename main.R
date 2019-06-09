039748a0eb79bb199750baeac3d422f05f80c6d2
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-06-09 13:57:06 -0400

    added sma function and basic plot

--- main.R
+++ main.R
@@ -12,8 +12,29 @@ import("ggplot2")
 source(file = "api_key.R")
 
 get_csv <- function(symbol) {
-	link <- paste("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=", symbol, "&datatype=csv&apikey=", api_key, sep = "")
-	link %>% read.csv %>% return
+	link <- paste("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=", symbol, "&datatype=csv&outputsize=full&apikey=", api_key, sep = "")
+	response <- link %>% read.csv
+	return(response[nrow(response) : 1,])
 }
 
-get_csv("TSLA") %>% head %>% print
+simple_moving_avg <- function(input, count = 20) {
+	acc <- 0
+	ret <- c()
+	for (i in 1:length(input)) {
+		acc <- acc + input[i]
+		if (i > count) {
+			acc <- acc - input[i - count]
+		}
+		ret[i] <- acc / min(i, count)
+	}
+	return(ret)
+}
+
+res <- get_csv("TSLA")
+res <- res %>%
+	select(timestamp, adjusted_close) %>%
+	rename(close = adjusted_close) %>%
+	mutate(moving_avg = simple_moving_avg(close))
+res %>% head %>% print
+# p1 <- ggplot() + geom_line(aes(x = as.numeric(row.names(res)), y = close), data = res)
+# plot(p1)
